003e165df680acd620df62b43308779ef9e38385
abdo-mhmd <abdo202224@gmail.com>
2023-09-07 16:06:04 +0300

    first stip

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/*
+ * main - main function
+ * @argc: get argument count
+ * @argv: get argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ shell.h
@@ -0,0 +1,8 @@
+#ifndef SHELL_H_C
+#define SHELL_H_C
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#endif
