e9c49ce49d29bfda58c5c02331dc475e9cb69ef9
abdo-elshaikh <132365018+abdo-elshaikh@users.noreply.github.com>
2023-09-07 17:24:08 +0300

    second

--- shell.h
+++ shell.h
@@ -4,5 +4,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <signal.h>
+#lnclude <errno.h>
+
+#define BUF_SIZE 1024
+#define TOK_BUF_SIZE 128
+#define TOK_DELIME "\t\r\n\a"
+
+extern char **environmevt;
+
+
+typedef struct data {
+    
+}
 
 #endif
